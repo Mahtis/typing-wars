2f619b228acf7d5b2e3f734f503a53561780d110
Mikko Tiainen <mikko.masala@gmail.com>
2020-04-23 20:27:20 +0300

    add reacting to receiving words and boards, and also add sending board

--- src/clientConnection.js
+++ src/clientConnection.js
@@ -1,44 +1,56 @@
-import _io from 'socket.io-client'
+import _io from 'socket.io-client';
 
 export const dependencies = {
   io: _io
-}
+};
 
-const { BACKEND_URL } = process.env
+const { BACKEND_URL } = process.env;
 
-const clientConnection = () => {
+const clientConnection = addWord => {
   let socket;
+  let updateOpponentBoard = () => {};
 
   const getRoomName = () => {
-    const path = document.location.pathname.split('/')
-    const roomPathIndex = path.findIndex(item => item === 'room') + 1
-    return path[roomPathIndex]
-  }
+    const path = document.location.pathname.split('/');
+    const roomPathIndex = path.findIndex(item => item === 'room') + 1;
+    return path[roomPathIndex];
+  };
 
   const initConnection = () => {
     const room = getRoomName();
 
-    socket = dependencies.io(BACKEND_URL)
+    socket = dependencies.io(BACKEND_URL);
     socket.emit('joinroom', room);
-    
+
     socket.on('msg', msg => {
-      console.log('hello hellooooo')
-      const li = document.createElement('li')
-      const text = document.createTextNode(msg)
-      li.appendChild(text)
-      messages.appendChild(li)
-    })
+      // console.log('hello hellooooo')
+      // const li = document.createElement('li')
+      // const text = document.createTextNode(msg)
+      // li.appendChild(text)
+      // messages.appendChild(li)
+      addWord(msg);
+    });
 
-    socket.on('error', error => console.log(error))
-  }
+    socket.on('error', error => console.log(error));
 
-  const getSocket = () => socket
+    socket.on('board', board => updateOpponentBoard(board))
+  };
 
-  const sendMessage = message => {
-    socket.emit('msg', message)
-  }
+  const sendBoard = board => {
+    socket.emit('board', board);
+  };
   
-  return { getSocket, initConnection, sendMessage }
-}
+  const sendMessage = message => {
+    socket.emit('msg', message);
+  };
+  
+  const getSocket = () => socket;
 
-export default clientConnection
+  const setOpponentBoardUpdater = updater => {
+    updateOpponentBoard = updater;
+  }
+
+  return { getSocket, initConnection, sendMessage, sendBoard, setOpponentBoardUpdater };
+};
+
+export default clientConnection;
--- src/clientConnection.test.js
+++ src/clientConnection.test.js
@@ -4,6 +4,7 @@ describe('clientConnection', () => {
   describe('when initializing connection', () => {
     let connection;
     let socketMock;
+    let addWordMock;
 
     beforeEach(() => {
       window.history.replaceState({}, 'Test Title', '/room/someroom');
@@ -13,17 +14,48 @@ describe('clientConnection', () => {
         on: jest.fn()
       };
 
+      addWordMock = jest.fn();
+
       dependencies.io = () => socketMock;
 
-      connection = clientConnection();
+      connection = clientConnection(addWordMock);
+
+      connection.initConnection();
     });
 
     it('joins a room based on path', () => {
-      connection.initConnection();
-
       expect(socketMock.emit).toHaveBeenCalledWith('joinroom', 'someroom');
     });
 
+    describe('given sending a message', () => {
+      beforeEach(() => {
+        connection.sendMessage('word');
+      });
 
+      it('emits message with msg header', () => {
+        expect(socketMock.emit).toHaveBeenCalledWith('msg', 'word')
+      })
+    })
+    
+    describe('given sending the word board', () => {
+      beforeEach(() => {
+        connection.sendBoard([
+          '      some',
+          '   board  ',
+          'with      ',
+          '  words   ']);
+      });
+
+      it('emits board with board header', () => {
+        expect(socketMock.emit).toHaveBeenCalledWith('board', [
+          '      some',
+          '   board  ',
+          'with      ',
+          '  words   '])
+      })
+    })
+
+    // TODO: Invent a way to test that socket.on() works,
+    // or should it be abstracted even further?
   });
 });
