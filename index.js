a0b054a0467391fdda627728e59a5669819e5ef8
Mikko Tiainen <mikko.masala@gmail.com>
2020-04-28 16:37:58 +0300

    add simple path for requesting a word list
    Note also added cors so that works locally when running front and back separately

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const _io = require('socket.io')
 const http = require('http')
+const cors = require('cors')
 const PORT = 3000
 
 const app = express()
@@ -9,13 +10,26 @@ const io = _io(server)
 
 const rooms = ['room1', 'room2', 'room3', 'room4', 'room5']
 
+const apiService = require('./apiService').apiService();
+
+app.use(cors())
+
 app.get('/asd', (req, res) => {
   io.in('/room/1').clients((error, clients) => {
     res.send('hello ' + clients.length)
   })
 })
+
 app.use('/', express.static(__dirname + '/dist', { 'index': 'room.html' }));
+
 app.use('/room/:room', express.static(__dirname + '/dist', { 'index': 'room.html' }));
+
+app.get('/api/words', (req, res) => {
+  const returnWords = apiService.getEnglishWords(500, 20)
+  console.log('got request: ' + req);
+  
+  res.json(returnWords)
+})
 
 io.on('connection', (socket) => {
   socket.on('chat message', function(msg){
@@ -30,12 +44,14 @@ io.on('connection', socket => {
     // do room exist check and number of clients check here
     // and if no-go, never even join the room
     socket.join(room)
+
     io.in(room).clients((error, clients) => {
       console.log(clients)
       if (clients.length > 2) {
         socket.disconnect(true)
       }
     })
+
     socket.on('msg', msg => {
       console.log(msg)
       // socket.emit menee lähettäjälle itselle pelkästään
