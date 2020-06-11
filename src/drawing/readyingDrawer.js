5195e0eebede5c6cbac9361f1f9f80a3bb1d39fa
Mikko Tiainen <mikko.masala@gmail.com>
2020-06-11 10:02:39 +0300

    add drawer for ready state

---
+++ src/drawing/readyingDrawer.js
@@ -0,0 +1,43 @@
+export default ctx => {
+  const { height, width } = ctx.canvas;
+
+  const draw = (playerReady, opponentReady) => {
+    const origFill = ctx.fillStyle;
+    ctx.clearRect(0, 0, width, height);
+    ctx.fillStyle = 'black';
+    ctx.fillText(
+      "Press Enter os Space when you're ready to start",
+      width / 2,
+      height / 2
+    );
+    ctx.fillText(
+      "YOU: ",
+      width / 2,
+      height / 2 + 20
+    );
+    ctx.fillStyle = playerReady ? 'green' : 'red'
+    ctx.fillText(
+      playerReady ? 'READY' : 'NOT READY',
+      width / 2 + 50,
+      height / 2 + 20
+    );
+
+    ctx.fillStyle = 'black';
+
+    ctx.fillText(
+      "OPPONENT: ",
+      width / 2,
+      height / 2 + 40
+    );
+    ctx.fillStyle = opponentReady ? 'green' : 'red'
+    ctx.fillText(
+      opponentReady ? 'READY' : 'NOT READY',
+      width / 2 + 100,
+      height / 2 + 40
+    );
+    
+    ctx.fillStyle = origFill;
+  };
+
+  return { draw };
+};
